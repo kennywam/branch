170403a353b7d9e1af104b948d41a99cccfe8f37
Kennedy Wambua <kennywambu254@gmail.com>
2023-12-05 17:28:35 +0300

    updates

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/branch', {
   useUnifiedTopology: true,
 });
 
-// Adjust the schema to match the MongoDB field names
 const messagingSchema = new mongoose.Schema({
-  userId: String, // Adjust to match the field name in your MongoDB collection
+  userId: String,
   messageBody: String,
   timestamp: { type: Date, default: Date.now },
+  isResponse: { type: Boolean, default: false },
 });
 
 const Message = mongoose.model('Message', messagingSchema);
@@ -36,35 +36,57 @@ app.use(bodyParser.json());
 app.get('/messages', async (req, res) => {
   try {
     const { userId } = req.query;
-    console.log('Received request for messages with userId:', userId);
 
-    const messages = await Message.find({ userId }).sort({ timestamp: 1 });
-    console.log('Retrieved messages:', messages);
+    // Fetch both messages and responses
+    const messages = await Message.find({ userId, isResponse: { $ne: true } }).sort({ timestamp: 1 });
+    const responses = await Message.find({ userId, isResponse: true }).sort({ timestamp: 1 });
 
-    res.json(messages);
+    const allMessages = [...messages, ...responses];
+
+    res.json(allMessages);
   } catch (error) {
     console.error('Error fetching messages:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+app.post('/send-response', async (req, res) => {
+  try {
+    const { userId, responseBody } = req.body;
+    const newResponse = new Message({ userId, messageBody: responseBody, isResponse: true });
+    await newResponse.save();
 
-app.post('/respond', async (req, res) => {
-  const { userId, messageBody } = req.body;
+    io.emit('newResponse', newResponse);
 
-  const newMessage = new Message({ 'User ID': userId, messageBody });
-  await newMessage.save();
-
-  io.emit('newMessage', newMessage);
-
-  res.json({ success: true, message: 'Response sent successfully.' });
+    res.json({ success: true, message: 'Response sent successfully.' });
+  } catch (error) {
+    console.error('Error sending response:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
+  });
+
+  socket.on('sendMessage', async (data) => {
+    const { userId, messageBody } = data;
+    const newMessage = new Message({ userId, messageBody });
+    await newMessage.save();
+
+    io.emit('newMessage', newMessage);
+  });
+
+  socket.on('sendResponse', async (data) => {
+    const { userId, responseBody } = data;
+    const newResponse = new Message({ userId, messageBody: responseBody });
+    await newResponse.save();
+
+    io.emit('newResponse', newResponse);
   });
 });
 
